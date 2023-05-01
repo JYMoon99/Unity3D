18c6dfdc1262dea00e8fdf06b959b0aa3ec496e1
JYMoon99 <97581974+JYMoon99@users.noreply.github.com>
2023-05-01 19:42:12 +0900

    (2023. 4. 20)

--- SBSGame/Assets/Sound/Script/SoundControl.cs
+++ SBSGame/Assets/Sound/Script/SoundControl.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SoundControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5.0f;
+    private CharacterController control;
+
     void Start()
     {
-        
+        control = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal"); // A-D, left, right -1 ~ 1 사이의 값을 반환하는 함수
+        float z = Input.GetAxis("Vertical"); // W-S, up key, down key -1 ~ 1 사이의 값을 반환하는 함수
+
+        Vector2 direction = new Vector3(x, 0, z).normalized; // 벡터의 정규화
+
+        control.SimpleMove(direction * speed); // SimpleMove : 중력 계산이 되는 함수
     }
 }
--- SBSGame/Assets/Sound/Script/SoundManager.cs
+++ SBSGame/Assets/Sound/Script/SoundManager.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public enum soundName
+    {
+        Siren,
+        Explosion,
+        Magic
+    }
+
     [SerializeField] AudioClip[] audioClip; // 여러 개의 사운드 클립을 담아야 하기에 배열로 선언
 
     [SerializeField] AudioSource soundSource; // 배경음악과 효과음을 따로 설정하기 위해서 AudioSource 2개 선언
@@ -11,6 +18,12 @@ public class SoundManager : MonoBehaviour
 
     public void SoundCall(int count)
     {
+        effectSource.PlayOneShot(audioClip[count]); // 리스너를 여러개 동시에 호출하고 싶을 때는 PlayOneShot
+    }
 
+    public void Volume(float volume)
+    {
+
+        soundSource.volume = volume;
     }
 }
