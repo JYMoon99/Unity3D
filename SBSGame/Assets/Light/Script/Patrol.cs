23f35e2de88a530f91e19c88e9cfab52faf1b6a4
JYMoon99 <97581974+JYMoon99@users.noreply.github.com>
2023-05-18 17:32:51 +0900

    (2023. 4. 25) Light

---
+++ SBSGame/Assets/Light/Script/Patrol.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    private Vector3 direction = Vector3.forward; // forward - 자신이 바라보고 있는 방향
+
+
+    void Update()
+    {
+        transform.Translate(direction * Time.deltaTime);
+
+        if(transform.position.x >= 2.5f)
+        {
+            direction = Vector3.forward;
+            transform.localScale = new Vector3(3.5f, 3.5f, 3.5f);
+        }
+        else if (transform.position.x <= -2.5f)
+        {
+            direction = Vector3.back;
+            transform.localScale = new Vector3(3.5f, 3.5f, -3.5f);
+        }
+
+        
+    }
+}
